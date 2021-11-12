eb116dc7331d36d92988d968240001b36050a29b
Jake Cooper <jc2088@kent.ac.uk>
2021-11-12 01:11:57 +0000

    Added date picker and DatePickerFragment.kt for general use Added additional options buttons Added next navigation

--- app/src/main/java/com/example/android/healthyhome/ui/FilterServicesFragment.kt
+++ app/src/main/java/com/example/android/healthyhome/ui/FilterServicesFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.healthyhome.R
 import com.example.android.healthyhome.databinding.FragmentFilterServicesBinding
 import com.example.android.healthyhome.databinding.FragmentProviderHomeBinding
@@ -16,9 +19,15 @@ class FilterServicesFragment : Fragment() {
     private lateinit var binding : FragmentFilterServicesBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-     // Inflate the layout for this fragment
+        // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate<FragmentFilterServicesBinding>(inflater, R.layout.fragment_filter_services, container, false)
-            return binding.root
+
+
+        binding.btnNext.setOnClickListener { view: View ->
+            view.findNavController().navigate(FilterServicesFragmentDirections.actionFilterServicesFragmentToFilterDateFragment())
         }
+
+        return binding.root
+    }
 
 }
