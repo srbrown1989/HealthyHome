212b04cfedc2162bf2d4e864d36884fc3deecfa8
irfan <mit7@kent.ac.uk>
2022-02-11 16:51:42 +0000

    update

---
+++ app/src/main/java/com/example/android/healthyhome/ui/ProviderBookingsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.android.healthyhome.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.healthyhome.R
+
+class ProviderBookingsFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_provider_bookings_fragment)
+    }
+}
