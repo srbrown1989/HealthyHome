33e122692259abed239b1b2a92a0ece8301dc9fa
irfan <mit7@kent.ac.uk>
2022-03-03 16:13:48 +0000

    provider phase 1

--- app/src/main/java/com/example/android/healthyhome/ui/ProviderBookingsFragment.kt
+++ app/src/main/java/com/example/android/healthyhome/ui/ProviderBookingsFragment.kt
@@ -2,13 +2,44 @@ package com.example.android.healthyhome.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.DatePicker
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.healthyhome.R
+import com.example.android.healthyhome.databinding.FragmentProviderSignUpBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-class ProviderBookingsFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_provider_bookings_fragment)
+class ProviderBookingsFragment : Fragment() {
+    private lateinit var binding: FragmentProviderSignUpBinding
+    private lateinit var database: DatabaseReference
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate<FragmentProviderSignUpBinding>(
+            inflater, R.layout.fragment_provider_sign_up, container, false
+        )
+        database = FirebaseDatabase.getInstance().reference.child("Provider")
+
+        binding.finishButton.setOnClickListener {
+            it.findNavController().navigate(ProviderSignUpFragmentDirections.actionProviderSignUpFragmentToProviderHomeFragment())
+
+        }
+
+        binding.backButton.setOnClickListener {
+            it.findNavController().navigate(ProviderSignUpFragmentDirections.actionProviderSignUpFragmentToServicesFragment())
+        }
+
+
+        return binding.root
     }
+
 }
 
 //fun providerTable(): String {
