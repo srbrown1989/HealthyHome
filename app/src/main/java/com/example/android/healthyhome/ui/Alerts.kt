bd6b85b2581ecd515a2d15324427281578119e44
Jake Cooper <jc2088@kent.ac.uk>
2022-03-29 16:08:17 +0100

    Confirmation Alert implemented

--- app/src/main/java/com/example/android/healthyhome/ui/Alerts.kt
+++ app/src/main/java/com/example/android/healthyhome/ui/Alerts.kt
@@ -3,7 +3,6 @@ package com.example.android.healthyhome.ui
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
-import android.view.View
 
 class Alerts {
     companion object{
